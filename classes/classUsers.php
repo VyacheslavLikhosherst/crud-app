5ed6ff09a15da83c4b421a43c9d2c8e368ff54f2
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2021-05-27 19:48:08 +0300

    Added search query

--- classes/classUsers.php
+++ classes/classUsers.php
@@ -4,16 +4,24 @@ require_once "connection.php";
 class Users extends Connection {
     public $link;
     public $query = "SELECT * FROM users";
-    public $result;
+    public $querySearch;
+    public $resultSearch;
+    public $resultUsers;
     function connect() {
         // Database connection
         $this->link = mysqli_connect($this->host, $this->user, $this->password, $this->database)
         or die("Ошибка " . mysqli_error($this->link));
     }
 
-    function request() {
+    function requestAllUsers() {
         // Executing a request
-        $this->result = mysqli_query($this->link, $this->query) or die("Ошибка " . mysqli_error($this->link));
+        $this->resultUsers = mysqli_query($this->link, $this->query) or die("Ошибка " . mysqli_error($this->link));
+    }
+
+    function requestSearchUsers($search) {
+        // Record Search Request
+        $this->querySearch = "SELECT * FROM users WHERE `id` LIKE '%$search%' OR `name` LIKE '%$search%' OR `age` LIKE '%$search%' OR `email` LIKE '%$search%'";
+        $this->resultSearch = mysqli_query($this->link, $this->querySearch) or die("Ошибка !!!" . mysqli_error($this->link));
     }
 
     function close() {
