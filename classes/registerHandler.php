2fc03becb232981752a43c4d8b7a2c43a498023f
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2021-05-31 22:57:57 +0300

    Made the user registration functionality

---
+++ classes/registerHandler.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+require_once "connection.php";
+
+
+class Register extends Connection {
+    public $fullName;
+    public $email;
+    public $login;
+    public $pass;
+    public $passRepeat;
+    public $link;
+    public $query;
+
+    // Class constructor
+    function __construct($fullName, $email, $login, $pass, $passRepeat)
+    {
+        $this->fullName = $fullName;
+        $this->email = $email;
+        $this->login = $login;
+        $this->pass = $pass;
+        $this->passRepeat = $passRepeat;
+        $this->query = "INSERT INTO `registered_users` (`full_name`, `email`, `login`, `password`)
+    VALUES ('$this->fullName', '$this->email', '$this->login', '$this->pass')";
+    }
+
+    // Database connection
+    function connect() {
+        $this->link = mysqli_connect($this->host, $this->user, $this->password, $this->database)
+        or die("Ошибка: " . mysqli_error($this->link));
+    }
+
+    // User registration request
+    function requestRegister() {
+        if($this->pass == $this->passRepeat) {
+            $result = mysqli_query($this->link, $this->query);
+            if($result) {
+                header('Location: ../login.php');
+            } else {
+                echo "Произошла какая-то ошибка!";
+            }
+        } else {
+            $_SESSION['message'] = 'Пароли не совпадают...';
+            header('Location: ../register.php');
+        }
+    }
+
+    // Closing a connection
+    function close() {
+        mysqli_close($this->link);
+    }
+}
+
+$fullName = str_replace("'", '', htmlspecialchars($_POST['full-name']));
+$email = str_replace("'", '',htmlspecialchars($_POST['email']));
+$login = str_replace("'", '',htmlspecialchars($_POST['login']));
+$pass = str_replace("'", '',htmlspecialchars($_POST['password']));
+$passRepeat = str_replace("'", '',htmlspecialchars($_POST['password-repeat']));
+
+$register = new Register($fullName, $email, $login, $pass, $passRepeat);
+$register->connect();
+$register->requestRegister();
+$register->close();
+
+
+
---
+++ register.php
@@ -0,0 +1,49 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/register.css">
+    <title>Document</title>
+</head>
+<body>
+<!-- Форма регистрации -->
+<form class="register-form" action="classes/registerHandler.php" method="post">
+    <h1>Регистрация</h1>
+    <div class="mb-3">
+        <label for="fullName" class="form-label" >Полное имя:</label>
+        <input type="text" required class="form-control" id="fullName" name="full-name">
+        <div id="emailHelp" class="form-text">Подсказка</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Введите ваш email:</label>
+        <input name="email" required type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="inputLogin" class="form-label">Придумайте логин:</label>
+        <input type="text" required class="form-control" id="inputLogin" name="login">
+        <div id="emailHelp" class="form-text">Подсказка</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Введите пароль:</label>
+        <input type="password" required class="form-control" id="exampleInputPassword1" name="password">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword2" class="form-label" >Повторите пароль:</label>
+        <input type="password" required class="form-control" id="exampleInputPassword2" name="password-repeat">
+    </div>
+    <button type="submit" class="btn btn-primary">Регистрация</button>
+    <p><strong>У вас есть аккаунт? - <a href="login.php">Войти</a>.</strong></p>
+    <?php
+    if($_SESSION['message']) {
+        echo "<div class=\"alert alert-danger\" role=\"alert\">$_SESSION[message]</div>";
+        unset($_SESSION['message']);
+    }
+    ?>
+</form>
+</body>
+</html>
