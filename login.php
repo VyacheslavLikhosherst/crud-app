46510893ea495906d458d974049429b601275bf0
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2021-06-14 21:50:02 +0300

    Commented out a block of code

--- login.php
+++ login.php
@@ -33,43 +33,43 @@ if($_SESSION['user']) {
     </div>
     <button type="submit" class="btn btn-primary">Войти</button>
     <p><strong>Вы не зарегистрированы? - <a href="register.php">Регистрация</a>.</strong></p>
-    <?php
-
-    require_once 'vendor/autoload.php';
-
-    // init configuration
-    $clientID = '905762750898-0mcmvggdpg7flpu2fl8jf4iqp9n1d2dq.apps.googleusercontent.com';
-    $clientSecret = 'N_6my7ub_rDg3QJhOehgc4JM';
-    $redirectUri = 'http://localhost/login.php';
-
-    // create Client Request to access Google API
-    $client = new Google_Client();
-    $client->setClientId($clientID);
-    $client->setClientSecret($clientSecret);
-    $client->setRedirectUri($redirectUri);
-    $client->addScope("email");
-    $client->addScope("profile");
-
-    if (isset($_GET['code'])) {
-        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-        $client->setAccessToken($token['access_token']);
-
-        // get profile in   fo
-        $google_oauth = new Google_Service_Oauth2($client);
-        $google_account_info = $google_oauth->userinfo->get();
-        $email = $google_account_info->email;
-        $name = $google_account_info->name;
-        $nameAc = $name;
-
-        $_SESSION['user'] = true;
-        $_SESSION['email'] = $email;
-
-        // now you can use this profile info to create account in your website and make user logged in.
-    } else {
-        echo "<a href='" . $client->createAuthUrl() . "'>Google Login</a>";
-    }
-
-    ?>
+<!--    --><?php
+//
+//    require_once 'vendor/autoload.php';
+//
+//    // init configuration
+//    $clientID = '905762750898-0mcmvggdpg7flpu2fl8jf4iqp9n1d2dq.apps.googleusercontent.com';
+//    $clientSecret = 'N_6my7ub_rDg3QJhOehgc4JM';
+//    $redirectUri = 'http://localhost/login.php';
+//
+//    // create Client Request to access Google API
+//    $client = new Google_Client();
+//    $client->setClientId($clientID);
+//    $client->setClientSecret($clientSecret);
+//    $client->setRedirectUri($redirectUri);
+//    $client->addScope("email");
+//    $client->addScope("profile");
+//
+//    if (isset($_GET['code'])) {
+//        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+//        $client->setAccessToken($token['access_token']);
+//
+//        // get profile in   fo
+//        $google_oauth = new Google_Service_Oauth2($client);
+//        $google_account_info = $google_oauth->userinfo->get();
+//        $email = $google_account_info->email;
+//        $name = $google_account_info->name;
+//        $nameAc = $name;
+//
+//        $_SESSION['user'] = true;
+//        $_SESSION['email'] = $email;
+//
+//        // now you can use this profile info to create account in your website and make user logged in.
+//    } else {
+//        echo "<a href='" . $client->createAuthUrl() . "'>Google Login</a>";
+//    }
+//
+//    ?>
     <?php
     if($_SESSION['message']) {
         echo "<div class=\"alert alert-danger\" role=\"alert\">$_SESSION[message]</div>";
