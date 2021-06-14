be38514ab284a8c0051c64c0ad7a2a1b50c4d581
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2021-06-14 21:44:42 +0300

    Added function to connect to database

--- classes/connection.php
+++ classes/connection.php
@@ -4,5 +4,17 @@ class Connection {
     public $database = 'crud'; // database name
     public $user = 'root'; // username
     public $password = 'root'; // password
+    public $link;
+
+    function connect() {
+        // Database connection
+        $this->link = mysqli_connect($this->host, $this->user, $this->password, $this->database)
+        or die("Ошибка " . mysqli_error($this->link));
+    }
+
+    function close() {
+        // Close
+        mysqli_close($this->link);
+    }
 }
 ?>
