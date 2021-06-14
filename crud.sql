49f679c0674cf0966a28a1502c623b0791881ad2
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2021-06-14 21:50:43 +0300

    Updated the database

--- crud.sql
+++ crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 31 2021 г., 23:00
+-- Время создания: Июн 14 2021 г., 21:31
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.1.33
 
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int NOT NULL,
+  `login_send` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login_get` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_user_send` int NOT NULL,
+  `id_user_get` int NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_time` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
+INSERT INTO `messages` (`id`, `login_send`, `login_get`, `id_user_send`, `id_user_get`, `text`, `date_time`) VALUES
+(1, 'slava', 'timur', 1, 2, 'Привет, Тимур!', '2021-06-14 21:26:14'),
+(2, 'timur', 'slava', 2, 1, 'Привет!', '2021-06-14 21:26:37'),
+(3, 'slava', 'timur', 1, 2, 'Чем занимаешься?', '2021-06-14 21:29:12');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `registered_users`
 --
 
@@ -40,9 +65,8 @@ CREATE TABLE `registered_users` (
 --
 
 INSERT INTO `registered_users` (`id`, `full_name`, `email`, `login`, `password`) VALUES
-(7, 'Вячеслав Лихошерст Павлович', 'test@gmail.com', 'slava', '123'),
-(9, 'Вячеслав Лихошерст Павлович', 'test@gmail.com', 'slava', '123'),
-(10, 'Кармыков Максим Игоревич', 'test@gmail.com', 'maxim', '123');
+(1, 'Вячеслав Лихошерст Павлович', 'guberslava@gmail.com', 'slava', '123'),
+(2, 'Тимур Танырбергенов Алтынбекович', 'timur@gmail.com', 'timur', '123');
 
 -- --------------------------------------------------------
 
@@ -63,17 +87,27 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `age`, `email`) VALUES
 (1, 'Андрей', 19, 'andrey@gmail.com'),
-(2, 'Виталий', 19, 'test@gmail.com');
+(2, 'Максим', 25, 'test@gmail.com'),
+(7, 'Тимур', 18, 'test@gmail.com');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `registered_users`
 --
 ALTER TABLE `registered_users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`),
+  ADD KEY `login` (`login`);
 
 --
 -- Индексы таблицы `users`
@@ -86,16 +120,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT для таблицы `registered_users`
 --
 ALTER TABLE `registered_users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
